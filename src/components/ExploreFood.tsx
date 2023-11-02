f8df58534a606379f0cbd0aea4734072e4d8e282
DipakDai <dipakkumal673@gmail.com>
2023-11-02 22:09:33 +0545

    feat: img data added

--- src/components/ExploreFood.tsx
+++ src/components/ExploreFood.tsx
@@ -1,51 +1,49 @@
+import { useEffect, useState } from "react";
+import { imgData } from "../data/Data";
 
-
+type ImgDataType = {
+  id: number;
+  imgUrl: string;
+  category: string;
+  title: string;
+};
 const ExploreFood = () => {
+  const [data, setData] = useState<ImgDataType[]>([]);
+
+  const filterData: ImgDataType[] = imgData.filter((data) => {
+    return data.category === "explore";
+  });
+
+  console.log(data, " img data");
+  useEffect(() => {
+    setData(filterData);
+  }, []);
   return (
     <section className="categories">
       <div className="container">
         <h2 className="text-center">Explore Foods</h2>
 
-        <a href="category-foods.html">
-          <div className="box-3 float-container">
-            <img
-              src="https://media-cdn.tripadvisor.com/media/photo-s/15/2d/23/07/domino-s-pizza.jpg"
-              alt="Pizza"
-              className="img-responsive img-curve"
-            />
+        {data.map(({ id, title, category, imgUrl }) => {
+          return (
+            <a href="category-foods.html" key={id}>
+              <div className="box-3 float-container">
+                <img
+                  src={imgUrl}
+                  alt={title}
+                  className="img-responsive img-curve"
+                />
 
-            <h3 className="float-text text-white">Pizza</h3>
-          </div>
-        </a>
-
-        <a href="#">
-          <div className="box-3 float-container">
-            <img
-              src="https://natashaskitchen.com/wp-content/uploads/2023/06/Cheeseburger.jpg"
-              alt="Burger"
-              className="img-responsive img-curve"
-            />
-
-            <h3 className="float-text text-white">Burger</h3>
-          </div>
-        </a>
-
-        <a href="#">
-          <div className="box-3 float-container">
-            <img
-              src="https://junifoods.com/wp-content/uploads/2023/07/Momo-chicken-khukura-recipe-500x500.png"
-              alt="Momo"
-              className="img-responsive img-curve"
-            />
-
-            <h3 className="float-text text-white">Momo</h3>
-          </div>
-        </a>
+                <h3 className="float-text text-white">{title}</h3>
+              </div>
+            </a>
+          );
+        })}
+       
         <div className="clearfix"></div>
       </div>
       <div className="explore_button">
-          <button>Explore More</button>
-        </div>
+        <button>Explore More</button>
+      </div>
     </section>
   );
 };
