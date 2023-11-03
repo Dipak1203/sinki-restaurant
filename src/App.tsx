2705eff8f7e2187ac9807c80586b889854aeea31
DipakDai <dipakkumal673@gmail.com>
2023-11-03 21:17:35 +0545

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/UI/HomePage";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ContactPage from "./components/UI/pages/ContactPage";
 const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,12 @@
 const Footer = () => {
   return (
     <section className="footer">
+      <div className="container">
+        <p className="mt-3"><strong>ADDRESS</strong>: Koteshwor-32, Mahadesthan, Kathmandu</p>
+        <p className="mt-3"><strong>CONTACT</strong>: +9779766916005</p>
+        <p className="mt-3"><strong>EMAIL</strong>: mailsinki@sinkrestaurant.com</p>
+        <p className="mt-3"><strong>FIND US</strong>: </p>
+      </div>
         <div className="container text-center">
             <p>All rights reserved. Designed By <a href="#">DIPAK KUMAL</a></p>
         </div>
