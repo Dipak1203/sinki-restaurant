2d336618805eb796309248dc9cb4ace55f23cbba
DipakDai <dipakkumal673@gmail.com>
2023-11-22 22:20:45 +0545

    feat: search the issue

--- src/components/FoodSearch.tsx
+++ src/components/FoodSearch.tsx
@@ -1,8 +1,34 @@
-const FoodSearch = () => {
+import React, { useState } from "react";
+import { imgData } from "../data/Data";
+
+type Search = {
+  product: string;
+};
+
+const FoodSearch: React.FC = () => {
+  const [data, setData] = useState(imgData);
+
+  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const searchTerm = formData.get("search") as string;
+
+    // console.log(searchTerm)
+
+    const filteredData = imgData.filter(
+      (item) => item.category.toLowerCase() === searchTerm.toLowerCase()
+    );
+
+    console.log(filteredData);
+
+    // setData(filteredData);
+  };
+
   return (
     <section className="food-search text-center">
       <div className="container">
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type="search"
             name="search"
@@ -11,7 +37,6 @@ const FoodSearch = () => {
           />
           <input
             type="submit"
-            name="submit"
             value="Search"
             className="btn ml-3 w-32 hover:cursor-pointer bg-[#ff4757] text-white"
           />
