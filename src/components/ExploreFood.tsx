169a04d2cecece7608061c7ffc4f2d35560e9d6c
DipakDai <dipakkumal673@gmail.com>
2023-11-24 16:03:21 +0545

    fix: search

--- src/components/ExploreFood.tsx
+++ src/components/ExploreFood.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { imgData } from "../data/Data";
 
 type ImgDataType = {
@@ -7,65 +7,68 @@ type ImgDataType = {
   category: string;
   title: string;
 };
+
 const ExploreFood = () => {
-  const [data, setData] = useState<ImgDataType[]>(imgData);
-  const [input,setInput] = useState('');
+  const [originalData, setOriginalData] = useState<ImgDataType[]>(imgData);
+  const [filteredData, setFilteredData] = useState<ImgDataType[]>(originalData);
+  const [input, setInput] = useState('');
 
-
-  const onSubmit = async(e:any) =>{
+  const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    const filterData = imgData.filter((value) =>{
+    const filterData = originalData.filter((value) => {
       return value.category === input;
-    })
+    });
 
-    setData(filterData)
-  }
-  
+    setFilteredData(filterData);
+  };
 
-  // console.log(input)
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = e.target.value;
+    setInput(inputValue);
 
-  // console.log(data, " data here...")
+    if (!inputValue) {
+      setFilteredData(originalData);
+    }
+  };
+
   return (
     <section className="categories">
-       <section className="food-search text-center">
-      <div className="container">
-        <form onSubmit={onSubmit}>
-          <input
-            type="search"
-            name="search"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="Search for Food.."
-            required
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn ml-3 w-32 hover:cursor-pointer bg-[#ff4757] text-white"
-          />
-        </form>
-      </div>
-    </section>
+      <section className="food-search text-center">
+        <div className="container">
+          <form onSubmit={onSubmit}>
+            <input
+              type="search"
+              name="search"
+              value={input}
+              onChange={onInputChange}
+              placeholder="Search for Food.."
+              required
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="btn ml-3 w-32 hover:cursor-pointer bg-[#ff4757] text-white"
+            />
+          </form>
+        </div>
+      </section>
       <div className="container">
         <h2 className="text-center">Explore Foods</h2>
 
-        {data.map(({ id, title, category, imgUrl }) => {
-          return (
-            <a href="category-foods.html" key={id}>
-              <div className="box-3 float-container">
-                <img
-                  src={imgUrl}
-                  alt={title}
-                  className="img-responsive img-curve"
-                />
+        {filteredData.map(({ id, title, category, imgUrl }) => (
+          <a href="category-foods.html" key={id}>
+            <div className="box-3 float-container">
+              <img
+                src={imgUrl}
+                alt={title}
+                className="img-responsive img-curve"
+              />
+              <h3 className="float-text text-white">{title}</h3>
+            </div>
+          </a>
+        ))}
 
-                <h3 className="float-text text-white">{title}</h3>
-              </div>
-            </a>
-          );
-        })}
-       
         <div className="clearfix"></div>
       </div>
       <div className="explore_button">
